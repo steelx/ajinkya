93383da81cc8cd294155dd4f772032955d4f8133
Ajinkya <ajinkyax@pm.me>
2023-02-19 20:59:36 +0530

    feat: chalk colors

--- index.js
+++ index.js
@@ -1,13 +1,34 @@
 #!/usr/bin/env node
-
-const welcome = require("cli-welcome");
+import welcome from "cli-welcome";
+import chalk from "chalk";
 
 welcome({
 	title: `Hello I'm Ajinkya Borade`,
-	tagLine: `Intro of a Node.js software developer`,
+	tagLine: `this is my small introduction!`,
 	bgColor: `#FADC00`,
 	color: `#000000`,
 	bold: true,
 	clear: true,
 	version: `0.0.1`
 });
+
+const log = console.log;
+const red = chalk.red;
+const green = chalk.green;
+const yellow = chalk.yellow;
+const redUnderline = chalk.rgb(123, 45, 67).underline;
+const boldText1 = chalk.hex('#DEADED').bold
+const twitterBlue = chalk.hex('#39A7DB').bold
+const githubGreen = chalk.hex('#25EF79').bold
+const youtubeRed = chalk.hex('#D52121').bold
+
+log(`Profession: ${red('Software Developer')}
+Experience: ${green('12 years+')}
+Skills: ${yellow('Javascript/Node.js, Typescript, C#')}`);
+
+// Use RGB colors in terminal emulators that support it.
+log(`
+👔 ${githubGreen('Github')}: ${redUnderline('https://github.com/steelx')}
+📥 ${twitterBlue('Twitter')}: ${redUnderline('https://twitter.com/ajinkyax')}
+🧲 ${youtubeRed('Youtube')}: ${redUnderline('https://youtube.com/ajinkyax')}
+`);

43a352984a31ecd2130456cc5e787c9c00e87b4b
Ajinkya <ajinkyax@pm.me>
2023-02-19 20:19:11 +0530

    feat: basic published to npm

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # ajinkya
+
+### Install
+
+```bash
+npm install ajinkya-cli -g
+```
+
+### Usage
+
+To use ajinkya-cli just type `ajinkya` in terminal.
---
+++ index.js
@@ -0,0 +1,13 @@
+#!/usr/bin/env node
+
+const welcome = require("cli-welcome");
+
+welcome({
+	title: `Hello I'm Ajinkya Borade`,
+	tagLine: `Intro of a Node.js software developer`,
+	bgColor: `#FADC00`,
+	color: `#000000`,
+	bold: true,
+	clear: true,
+	version: `0.0.1`
+});
